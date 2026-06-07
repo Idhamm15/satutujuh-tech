d84f205229331e56e1e83950da64cae83d6c0533
Idhamm15 <idhamtamvanz123@gmail.com>
2026-06-07 19:36:13 +0700

    update: connect google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,6 +17,23 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Satu Tujuh Tech",
   description: "Transform Your Business with Technology",
+
+  openGraph: {
+    title: "Satu Tujuh Tech",
+    description: "Transform Your Business with Technology",
+    url: "https://satutujuh.net",
+    siteName: "Satu Tujuh Tech",
+    images: [
+      {
+        url: "https://satutujuh.net/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Satu Tujuh Tech",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -27,7 +46,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
+
+      <GoogleAnalytics gaId="AW-18197696265" />
     </html>
   );
 }
